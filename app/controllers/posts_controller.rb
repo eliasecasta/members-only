22cda7663621c25870fca9fa2e9eb5acf53ac547
s0kil <dtechtours@gmail.com>
2020-09-24 17:35:55 -0400

    Added Index View With User Full Name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,27 @@ class PostsController < ApplicationController
   end
 
   def index
-    @post = Post.all
+    @posts = Post.all
+    @user  = current_user
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post         = Post.new(post_params)
+    user_id       = current_user.id
+    @post.user_id = user_id
+
     if @post.save
-      flash[:success] = "Post successfully created"
-      redirect_to @post
+      flash[:success] = 'Post successfully created'
+      redirect_to root_path
     else
-      flash[:error] = "Something went wrong"
+      flash[:error] = 'Something went wrong'
       render 'new'
     end
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'posts#new'
+  devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout' }
+  root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts, only: %i[new create index]
 end
