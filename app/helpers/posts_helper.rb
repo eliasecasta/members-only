986056c1721b1faf174cfa811a22e47a3406b1e2
s0kil <dtechtours@gmail.com>
2020-09-25 15:45:41 -0400

    Rubocop Suggestion

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -13,12 +13,12 @@ module PostsHelper
   end
 
   def show_author_if_user_session
-    if user_signed_in?
-      content_tag(:span) do
-        concat 'Author: '
-        concat @user.fullname
-        concat content_tag(:br)
-      end
+    return unless user_signed_in?
+
+    content_tag(:span) do
+      concat 'Author: '
+      concat @user.fullname
+      concat content_tag(:br)
     end
   end
 end
